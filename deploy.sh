8a83a4e352e2220c554803bdc596134737760e88
tyKot <furu1997@mail.ru>
2022-08-05 17:49:31 +0400

    add some file

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:tyKot/vue-pervomaiskaya.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:tyKot/vue-pervomaiskaya.git master:gh-pages
+git push -f https://github.com/tyKot/vue-pervomaiskaya.git master:gh-pages
 
 cd -
